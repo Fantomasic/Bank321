fbdeea65fad272e134a2adda8249f9b38c188e11
Davidson228 <davidarinsh@gmail.com>
2018-12-01 11:00:31 +0200

    Initial commit

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require 'connection.php';
+$app = new \atk4\ui\App('Money');
+$app->initLayout('Centered');
+$client=new Client($db);
+$client->load($_SESSION['id']);
+$friend=$client->ref('Friends');
+$crud=$app->add('CRUD');
+$crud->setModel($friend,['name','phone','email']);
+$crud->addDecorator('name',new \atk4\ui\TableColumn\Link('din.php?friends_id={$id}'));
