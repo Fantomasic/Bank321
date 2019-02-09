c9138670469bde923deca39e18f6c42bd70cf999
Davidson228 <davidarinsh@gmail.com>
2019-02-09 10:48:13 +0200

    Update

--- clicker.php
+++ clicker.php
@@ -3,21 +3,25 @@ session_start();
 require 'connection.php';
 $app = new \atk4\ui\App('Clicker');
 $app->initLayout('Centered');
-$button=$app->add(['Button','Click','purple']);
+
 //$button->on('click', function($action){
     //return $action->text(rand(1,100));
 //  });
-$button->on('click', function($action){
-  $_SESSION['i']=$_SESSION['i']+1;
-  return $action->text($_SESSION['i']);
-});
-if(!isset($_SESSION['timer'])){
+
+if(!isset($_SESSION['flag'])){
 $_SESSION['timer'] = time();
 
 }
 $now = time();
 $_SESSION['t'] = $now - $_SESSION['timer'];
+
+  if ($_SESSION['t']<=15)  ($_SESSION['click']==20) {
+    $_SESSION['t']=0;
+  }
+  
+$button=$app->add(['Button','Click','purple']);
 $button->on('click', function($action){
-  $_SESSION['i']=$_SESSION['i']+1;
-   return $action->text($_SESSION[t]);
+  $_SESSION['click']=$_SESSION['click']+1;
+  $_SESSION['flag']=true;
+    return $action->text($_SESSION['t']);
 });
--- home.php
+++ home.php
@@ -15,8 +15,7 @@ $button->link(['clicker']);
   for ($i = 1;$i= 13;$i++){
   $str = $str.rand(0,9);
   }
-  $new_account = new Account($db);
-  $new_account->client_id = $_SESSION['id'];
+    $new_account->client_id = $_SESSION['id'];
   $new_account->number = $str;
   $new_account->balance = 0;
   $new_account->save();
@@ -27,3 +26,6 @@ $account=$client->ref('Account');
 $grid=$app->add('Grid');
 $grid->setModel($account);
 $grid->addDecorator('account_number',new \atk4\ui\TableColumn\Link('account_overview.php?number_id={$id}'));
+unset($_SESSION['t']);
+unset($_SESSION['flag']);
+unset($_SESSION['timer']);
